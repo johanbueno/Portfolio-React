ed4f6bcff0c15d1661df4037560a26ba5a08cd58
Johan Bueno <johan.s.bueno@gmail.com>
2020-03-09 21:31:15 -0400

    NavBar

--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -5,64 +5,47 @@ import "./style.css";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        Pupster
-      </Link>
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link
-              to="/"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/about"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/discover"
-              className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
-            >
-              Discover
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/search"
-              className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
-            >
-              Search
-            </Link>
-          </li>
-        </ul>
+    <header>
+      <div id="mobile-menu-close">
+        <span>Close</span> <i className="fa fa-times" aria-hidden="true"></i>
       </div>
-    </nav>
+      <ul id="menu" className="shadow">
+        <li>
+          <Link className="navbar-brand" to="/">
+            About
+          </Link>
+
+        </li>
+        <li>
+          <Link
+            to="/"
+            className={
+              window.location.pathname === "/" || window.location.pathname === "/portfolio"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Portfolio
+          </Link>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <Link
+            to="/"
+            className={
+              window.location.pathname === "/" || window.location.pathname === "/contact"
+                ? "nav-link active"
+                : "nav-link"
+            } >
+            Contact
+            </Link>
+        </li>
+      </ul>
+    </header>
   );
 }
-// // my Nav
-// <header>
-//     <div id="mobile-menu-close">
-//       <span>Close</span> <i class="fa fa-times" aria-hidden="true"></i>
-//     </div>
-//     <ul id="menu" class="shadow">
-//       <li>
-//         <a href="#about">About</a>
-//       </li>
-//       <li>
-//         <a href="portfolio.html">Projects</a>
-//       </li>
-//       <li>
-//         <a href="#skills">Skills</a>
-//       </li>
-//       <li>
-//         <a href="contact.html">Contact</a>
-//       </li>
-//     </ul>
-//   </header>
+
 
 export default Navbar;
