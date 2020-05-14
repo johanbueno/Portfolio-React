b5e619c53076e66f25f2c13e269df3a46891b037
Johan Bueno <johan.s.bueno@gmail.com>
2020-05-13 22:36:09 -0400

    home

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,17 +1,22 @@
 import React from "react";
 import Johan from "../../images/JOHAN-B (1).webp"
 import "./style.css";
-import Angular from "../../images/angular.png";
-import Ajax from "../../images/ajax.png";
-import API from "../../images/api.jpeg";
-import Bootstrap from "../../images/bootstrap.png";
-import Css from "../../images/css.png";
-import Express from "../../images/Express.png";
-import JavaScript from "../../images/javascript.png";
-import Node from "../../images/node js.png";
-import Html from "../../images/html5.png";
-import MySQL from "../../images/MySQL.png";
-import Git from "../../images/git.png"
+import Angular from "../../images/angular.webp";
+import Ajax from "../../images/ajax.webp";
+import API from "../../images/API.webp";
+import Bootstrap from "../../images/bootstrap.webp";
+import Css from "../../images/css.webp";
+import Express from "../../images/Express.webp";
+import JavaScript from "../../images/javascript.webp";
+import Node from "../../images/node.webp";
+import Html from "../../images/html5.webp";
+import MySQL from "../../images/MySql.webp";
+import Git from "../../images/git.webp";
+import Mongodb from "../../images/mongodb.webp";
+import Reactjs from "../../images/reactjs.webp";
+import Responsive from "../../images/responsive.webp";
+import Jquery from "../../images/jquery1.webp"
+
 
 
 
@@ -20,94 +25,139 @@ function Home() {
     <div>
       <div id="lead">
         <div id="lead-content">
-          <h1>Johan Bueno</h1>
+          <h2>Johan Bueno</h2>
           <picture>
-            <source type="image/webp" srcset={Johan}/>
+            <source type="image/webp" srcset={Johan} />
 
-              <img id="Johan" src={Johan} alt="Johan-Bueno"/>
+            <img id="Johan" src={Johan} alt="Johan-Bueno" />
           </picture>
-              
-              <h2>Full Stack Developer</h2>
-              <a href="https://docs.google.com/document/d/1FMf7T5so_sac4FqvwdbPdpkXCgfbQ_DDcNWX3oh4fcg/edit?usp=sharing"
-                className="btn-rounded-white">Download Resume</a>
-         </div>
 
-            <div id="lead-down">
-              <span>
-                <i className="fas fa-arrow-circle-down"></i>
-              </span>
-            </div>
-
+          <h2>Full" Stack Developer</h2>
+          <a href="https://docs.google.com/document/d/1FMf7T5so_sac4FqvwdbPdpkXCgfbQ_DDcNWX3oh4fcg/edit?usp=sharing"
+            className="btn-rounded-white">Download Resume</a>
         </div>
-          <div className="container ">
+
+        <div id="lead-down">
+          <span>
+            <i className="fas fa-arrow-circle-down"></i>
+          </span>
+        </div>
+
+      </div>
+      <div className="container ">
+        <div className="row">
+          <div className="col-md-1">
+          </div>
+          <div className="col-md-10  center">
             <div className="row">
-              <div className="col-md-1">
-              </div>
-              <div className="col-md-10  center">
-                <div className="row">
-                  <div className="col-md-6">
+              <div className="col-md-6">
 
-                    <hr></hr>
+                <div id="about">
+                <p>Passionate Full Stack Web Developer with a background in Finance and Logistics with Certification from University of North Carolina. Life-long dedication to learning  including technical skills in HTML5, CSS3 Javascript, JQuery, Bootstrap, Node Js, MySQL, MongoDB, Express, Handelbars.js & ReactJS and Angular. Effective at combining creativity and problem solving to develop user friendly applications. Known among staff for being strong in detail and attention no matter the complexity of the project. Looking to bring my attention to detail and technical skills to a company to grow and become a valuable asset. </p>
 
-                    <div id="about">
-                      <p>Full Stack Web Developer with a Bachelor’s degree in Finance and Foreign exchange. Warehouse
-                      Manager of a Distribution center. Efficient on time management and conflict resolution. Developer of
-                      responsive web applications oriented on users needs. Looking for accountability on every single project. Known among staff for
-                   strong with attention to detail no matter the complexity of the project. </p>
-
-                    </div>
-
-                  </div>
-                  <div className="col-md-6   pics">
-                    <img src={Angular} alt="angular"></img>
-                    <img src={JavaScript} alt="javascript"></img>
-                    <img src={Ajax} alt="ajax"></img>
-                    <img src={API} alt="api"></img>
-                    <img src={Bootstrap} alt="bootstrap"></img>
-                    <img src={Node} alt="nodejs"></img>
-                    <img src={Git} alt="git"></img>
-                    <img src={Html} alt="html5"></img>
-                    <img src={Css} alt="css"></img>
-                    <img src={Express} alt="Express"></img>
-                    <img src={MySQL} alt="MySQL"></img>
-
-                  </div>
-                </div>
-                <div id="skills">
-                  <h2 className="heading">Skills</h2>
-                  <ul>
-                    <li>JavaScript</li>
-                    <li>Node.js</li>
-                    <li>Angular</li>
-                    <li>React</li>
-                    <li>JavaScript</li>
-                    <li>Github</li>
-                    <li>Node.js</li>
-                    <li>Express</li>
-                    <li>API</li>
-                    <li>MySQL</li>
-                    <li>Mongo</li>
-                    <li>Css</li>
-                    <li>Bootstrap</li>
-
-                  </ul>
                 </div>
 
               </div>
+              <div className="col-md-6   pics">
+                <picture>
+                  <source type="image/webp" srcset={Angular} />
 
-              <div className="col-md-1"></div>
+                  <img src={Angular} alt="angular" />
+                </picture>
+                <picture>
+                  <source type="image/webp" srcset={JavaScript} />
+                  <img src={JavaScript} alt="javascript" />
+                </picture>
+                <picture>
+                  <source type="image/webp" srcset={Ajax} />
+                  <img src={Ajax} alt="ajax" />
+                </picture>
+                <picture>
+                  <source type="image/webp" srcset={API} />
+                  <img src={API}  alt="API" />
+                </picture>
+                <picture>
+                  <source type="image/webp" srcset={Bootstrap} />
+                  <img src={Bootstrap} alt="Bootstrap" />
+                </picture>
+                <picture>
+                  <source type="image/webp" srcset={Node} />
+                  <img src={Node} alt="node" />
+                </picture>
+                <picture>
+                  <source type="image/webp" srcset={Git} />
+                  <img src={Git} alt="git" />
+                </picture>
+                <picture>
+                  <source type="image/webp" srcset={Html} />
+                  <img src={Html}  alt="html5" />
+                </picture>
+                <picture>
+                  <source type="image/webp" srcset={Css} />
+                  <img src={Css}  alt="css" />
+                </picture>
+                <picture>
+                  <source type="image/webp" srcset={Express} />
+                  <img src={Express} alt="Express" />
+                </picture>
+                <picture>
+                  <source type="image/webp" srcset={MySQL} />
+                  <img src={MySQL}  alt="MySQL" />
+                </picture>
+                <picture>
+                  <source type="image/webp" srcset={Mongodb}/>
+                  <img src={Mongodb} alt="mongodb" />
+                </picture>
+                <picture>
+                  <source type="image/webp" srcset={Reactjs} />
+                  <img src={Reactjs} alt="reactjs" />
+                </picture>
+                <picture>
+                  <source type="image/webp" srcset={Responsive} />
+                  <img src={Responsive} alt="responsive" />
+                </picture>
+                <picture>
+                  <source type="image/webp" srcset={Jquery} />
+                  <img src={Jquery} alt="jquery" />
+                </picture>
+
+              </div>
             </div>
-            <div id="to-top">
-              <span>
-                <i class Name="fas fa-arrow-circle-up"></i>
-              </span>
+            <div id="skills">
+              <h2 className="heading">Skills</h2>
+              <ul>
+                <li>JavaScript</li>
+                <li>Node.js</li>
+                <li>Angular</li>
+                <li>React</li>
+                <li>JavaScript</li>
+                <li>Github</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>API</li>
+                <li>MySQL</li>
+                <li>Mongo</li>
+                <li>Css</li>
+                <li>Bootstrap</li>
+
+              </ul>
             </div>
 
           </div>
 
+          <div className="col-md-1"></div>
+        </div>
+        <div id="to-top">
+          <span>
+            <i class Name="fas fa-arrow-circle-up"></i>
+          </span>
         </div>
 
-    );
+      </div>
+
+    </div>
+
+  );
 }
 
 export default Home 
